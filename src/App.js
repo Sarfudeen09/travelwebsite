23d816f569051beb080267fb907189e431d001be
sarfudeen09 <mohammedsarfudeen@gmail.com>
2024-03-27 17:18:19 +0530

    first commit2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/travelwebsite" element={<Home/>}/>
         <Route path="/About" element={<About/>}/>
         <Route path="/Service" element={<Service/>}/>
         <Route path="/Contact" element={<Contact/>}/>
